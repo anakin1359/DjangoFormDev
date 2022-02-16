baf293d8311bd806e97b78439965ced1a10d8da4
anakin1359 <anakin1359@gmail.com>
2022-02-17 02:11:06 +0900

    POST処理の実装に伴い、csrf_tokenを静的ファイルに追加

--- FormApp/views.py
+++ FormApp/views.py
@@ -6,6 +6,16 @@ def index(request):
 
 def form_page(request):
   form = forms.UserInfo()
+  if request.method == "POST":
+    # Formで送信されたデータを取得する
+    form = forms.UserInfo(request.POST)
+    
+    if form.is_valid():   # フィールの適正値検査（バリデーションの実装）
+      print("フォールドの内容に誤りはありません。")
+      print(
+        f"name: {form.cleaned_data['name']}, mail: {form.cleaned_data['mail']}, age: {form.cleaned_data['age']}"
+      )
+      
   return render(
     request, "formapp/form_page.html", context={
       "form": form
